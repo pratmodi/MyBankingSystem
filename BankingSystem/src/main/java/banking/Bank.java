12123c001d3f59f8b9330e53a2a7c4f2fe2b8075
pratmodi <pratmodi@gmail.com>
2022-12-22 13:21:56 +0530

    first commit

--- BankingSystem/src/main/java/banking/Bank.java
+++ BankingSystem/src/main/java/banking/Bank.java
@@ -1,6 +1,7 @@
 package banking;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Private Variables:<br>
@@ -10,40 +11,100 @@ public class Bank implements BankInterface {
 	private LinkedHashMap<Long, Account> accounts;
 
 	public Bank() {
-		// complete the function
+		accounts = new LinkedHashMap<>();
 	}
 
 	private Account getAccount(Long accountNumber) {
-		// complete the function
-        return null;
+		Account acc = null;
+
+		for(Map.Entry<Long, Account> entry:accounts.entrySet()){
+			if(entry.getValue().getAccountNumber()==accountNumber){
+				acc = entry.getValue();
+				break;
+			}
+		}
+
+        return acc;
 	}
 
 	public Long openCommercialAccount(Company company, int pin, double startingDeposit) {
-		// complete the function
-        return -1L;
+	Long accNo = 0L;
+		for(Map.Entry<Long, Account> entry:accounts.entrySet()){
+			if(entry.getValue().getAccountHolder().equals(company)){
+				accNo = entry.getValue().getAccountNumber();
+				break;
+			}
+		}
+
+        return accNo;
 	}
 
 	public Long openConsumerAccount(Person person, int pin, double startingDeposit) {
-		// complete the function
-        return -1L;
+
+		Long accNo = 0L;
+		for(Map.Entry<Long, Account> entry:accounts.entrySet()){
+			if(entry.getValue().getAccountHolder().equals(person)){
+				accNo = entry.getValue().getAccountNumber();
+				break;
+			}
+		}
+
+		return accNo;
+
 	}
 
 	public boolean authenticateUser(Long accountNumber, int pin) {
-		// complete the function
-        return true;
+		boolean flag = false;
+
+		Long accNo = 0L;
+		for(Map.Entry<Long, Account> entry:accounts.entrySet()){
+			if(entry.getValue().getAccountNumber()==accountNumber){
+				accNo = entry.getValue().getAccountNumber();
+				if(entry.getValue().validatePin(pin)){
+					flag = true;
+				}
+				break;
+			}
+		}
+
+		return flag;
+
 	}
 
 	public double getBalance(Long accountNumber) {
-		// complete the function
-        return -1;
+		double bal = 0L;
+		for(Map.Entry<Long, Account> entry:accounts.entrySet()){
+			if(entry.getValue().getAccountNumber()==accountNumber){
+				bal = entry.getValue().getBalance();
+				break;
+			}
+		}
+		return bal;
 	}
 
 	public void credit(Long accountNumber, double amount) {
-		// complete the function
+
+		for(Map.Entry<Long, Account> entry:accounts.entrySet()) {
+			if (entry.getValue().getAccountNumber() == accountNumber) {
+				this.accounts.get(accountNumber).creditAccount(amount);
+				break;
+			}
+
+		}
+
 	}
 
 	public boolean debit(Long accountNumber, double amount) {
-		// complete the function
-        return true;
+		boolean flag = false;
+		for(Map.Entry<Long, Account> entry:accounts.entrySet()) {
+			if (entry.getValue().getAccountNumber() == accountNumber) {
+				if(this.accounts.get(accountNumber).debitAccount(amount)) {
+					flag = true;
+				}
+				}
+				break;
+			}
+
+        return flag;
 	}
 }
